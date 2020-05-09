877f1b457d31f2ae143223375dcf4c6c9d87cab8
Elia Cereda <eliacereda@gmail.com>
2020-05-09 14:01:37 +0200

    Updated tests/dataset_visualization.py to generate videos for 10 random runs

--- tests/dataset_visualization.py
+++ tests/dataset_visualization.py
@@ -10,26 +10,31 @@ controller = "omniscient-controller"
 datasets_dir = "datasets/"
 runs_dir = os.path.join(datasets_dir, controller)
 dataset = load_dataset(runs_dir)
+
+# Select runs that we're interrupted before reaching the goal
 dataset.load()
-
 last_steps = dataset.groupby("run").map(lambda x: x.isel(sample=-1))
-runs = last_steps.run.where(last_steps.goal_reached == False)
+truncated_runs = last_steps.run.where(last_steps.goal_reached == False)
 
-run_id = np.random.choice(runs)
-run = dataset.where(dataset.run == run_id, drop=True)
 
-print("Generating video for run %d…" % run_id)
+n_runs = 10
 
-marxbot = viz.DatasetSource(run)
+for _ in range(n_runs):
+    run_id = np.random.choice(runs)
+    run = dataset.where(dataset.run == run_id, drop=True)
 
-# Create the visualizations
-env = viz.FuncAnimationEnv([
-    viz.GridLayout((1, 2), [
-        viz.LaserScannerViz(marxbot),
-        viz.ControlSignalsViz(marxbot)
-    ], suptitle="%s: run %d" % (controller, run_id))
-], sources=[marxbot])
-env.show(figsize=(9, 4))
+    print("Generating video for run %d…" % run_id)
 
-video_path = os.path.join(runs_dir, 'run-%d.mp4' % run_id)
-env.save(video_path, dpi=300)
+    marxbot = viz.DatasetSource(run)
+
+    # Create the visualizations
+    env = viz.FuncAnimationEnv([
+        viz.GridLayout((1, 2), [
+            viz.LaserScannerViz(marxbot),
+            viz.ControlSignalsViz(marxbot)
+        ], suptitle="%s: run %d" % (controller, run_id))
+    ], sources=[marxbot])
+    env.show(figsize=(9, 4))
+
+    video_path = os.path.join(runs_dir, 'run-%d.mp4' % run_id)
+    env.save(video_path, dpi=300)
